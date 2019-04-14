b75228a429ab64496419c54a451b47d4937f02bc
Brandt Bucher <brandtbucher@gmail.com>
2019-04-14 10:54:01 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         name="pycapi",
         url="https://github.com/brandtbucher/pycapi",
-        version="0.16.1",
+        version="0.17.0",
     )
