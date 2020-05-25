68fb40168b566680b7a2c1f192a511109105d03f
Brandt Bucher <brandtbucher@gmail.com>
2020-05-25 14:20:32 -0700

    Fix clang support.

--- generate.py
+++ generate.py
@@ -973,7 +973,7 @@ RETURN_TYPES_C = {
 
 CONVERTERS = {
     "": "Py_RETURN_NONE;",
-    "!": "abort();",
+    "!": "assert(0);",
     "C": 'return PyUnicode_FromFormat("%c", result);',
     "D": "return PyComplex_FromCComplex(result);",
     "d": "return PyFloat_FromDouble(result);",
